32b5f029a1602c8e8a61c8f439cb5caba04d31a1
Kalyani <kalyani@kalyanis-mac-pro.local>
2016-09-13 09:59:42 +0530

    Initial Commit

---
+++ kwColourMixing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kwColourMixing
+//
+//  Created by Kalyani on 13/09/16.
+//  Copyright © 2016 Kalyani Warke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kwColourMixing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kwColourMixing
+//
+//  Created by Kalyani on 13/09/16.
+//  Copyright © 2016 Kalyani Warke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
